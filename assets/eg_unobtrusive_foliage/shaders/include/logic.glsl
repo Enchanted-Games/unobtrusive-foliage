31e8421a80aa3a8c7af788c93f32796e796554ca
Enchanted-Games <isaacoblackshaw@gmail.com>
2025-07-19 00:50:09 +0100

    initial

---
+++ assets/eg_unobtrusive_foliage/shaders/include/logic.glsl
@@ -0,0 +1,43 @@
+#define FALLOFF_RANGE 5.0
+#define FALLOFF_START 2.0
+
+#define DITHER_PIXEL_SIZE 1.0
+#define DITHER_NOISE_SIZE 120.0
+#define DITHER_NOISE_ORIGINAL_BIAS 40.0
+#define DITHER_NOISE_ANIMATION_SPEED 1200.0
+
+#moj_import <eg_unobtrusive_foliage:noise.glsl>
+
+const mat4 dither_matrix = mat4(
+    // https://discourse.urho3d.io/t/screen-door-transparency/5054/2
+    1.0 / 17.0,  9.0 / 17.0,  3.0 / 17.0,  11.0 / 17.0,
+    13.0 / 17.0, 5.0 / 17.0,  15.0 / 17.0, 7.0 / 17.0,
+    4.0 / 17.0,  12.0 / 17.0, 2.0 / 17.0,  10.0 / 17.0,
+    16.0 / 17.0, 8.0 / 17.0,  14.0 / 17.0, 6.0 / 17.0
+);
+
+void applyDitherMatrix(float dist, vec2 fragCoord) {
+    // some devices may have performance issues if a discard statement is present
+    // in the solid pipeline (even if its never used), so its wrapped in a
+    // define just to be safe
+    #ifdef EG_USE_DISCARD
+    int x = int(fragCoord.x);
+    int y = int(fragCoord.y);
+    if (dist < dither_matrix[x % 4][y % 4])
+        discard;
+    #endif
+}
+
+void applyDistanceDither(sampler2D tex, vec2 texCoord, vec3 vertexPos, vec2 screenCoord) {
+    int alpha = int(floor(texture(tex, texCoord).a * 255.0));
+    if(alpha == 249) {
+        float aspect = ScreenSize.x / ScreenSize.y;
+        float noise = fractalNoise((gl_FragCoord.xy / ScreenSize) * DITHER_NOISE_SIZE * vec2(aspect, 1.0) + (GameTime * DITHER_NOISE_ANIMATION_SPEED));
+
+        float fragDist = length(vertexPos);
+
+        // fragDist += clamp(noise / DITHER_NOISE_ORIGINAL_BIAS, 0.0, 1.0);
+
+        applyDitherMatrix((fragDist * float(FALLOFF_RANGE)) - float(FALLOFF_START), screenCoord.xy / float(DITHER_PIXEL_SIZE));
+    }
+}
---
+++ assets/minecraft/shaders/core/terrain.fsh
@@ -0,0 +1,34 @@
+#version 150
+
+#moj_import <minecraft:fog.glsl>
+#moj_import <minecraft:dynamictransforms.glsl>
+#moj_import <minecraft:globals.glsl>
+
+#ifdef ALPHA_CUTOUT
+#define EG_USE_DISCARD
+#endif
+#moj_import <eg_unobtrusive_foliage:logic.glsl>
+
+uniform sampler2D Sampler0;
+
+in float sphericalVertexDistance;
+in float cylindricalVertexDistance;
+in vec4 vertexColor;
+in vec2 texCoord0;
+
+out vec4 fragColor;
+
+in vec3 vertPos;
+
+void main() {
+    vec4 color = texture(Sampler0, texCoord0) * vertexColor * ColorModulator;
+#ifdef ALPHA_CUTOUT
+    if (color.a < ALPHA_CUTOUT) {
+        discard;
+    }
+#endif
+    
+    applyDistanceDither(Sampler0, texCoord0, vertPos, gl_FragCoord.xy);
+
+    fragColor = apply_fog(color, sphericalVertexDistance, cylindricalVertexDistance, FogEnvironmentalStart, FogEnvironmentalEnd, FogRenderDistanceStart, FogRenderDistanceEnd, FogColor);
+}
